9904a40ad4c970ed395b1ea5eda5d7c35df083dd
rajaee <s.rajaee@3f.com>
2025-05-06 15:42:49 +0330

    fix business logic config SecurityConfig
    filter should use jti id

---
+++ src/main/java/com/shop/api_gateway/dto/enumDto/EnumResult.java
@@ -0,0 +1,28 @@
+package com.shop.api_gateway.dto.enumDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EnumResult {
+    USER_NOT_FOUND("user Not Found","100"),
+    EMAIL_IS_USED("Email already in use","101"),
+    USER_IS_USED("Username already in use","101"),
+    PHONE_IS_USED("PhoneNumber already in use","101"),
+    ERROR_CREATE_ACCOUNT("Error creating account","500"),
+    AUTHENTICATION_FAILED("Authentication failed","101"),
+    BAD_CREDENTIALS("Bad credentials","400"),
+    ACCOUNT_DISABLED("Account disabled","103"),
+    INTERNAL_SERVER_ERROR("Internal Server Error","500"),
+
+
+    FORBIDDEN("Forbidden","403"),
+
+    ;
+
+
+
+    private final String message;
+    private final String code;
+}
---
+++ src/main/java/com/shop/api_gateway/entity/log/LogEntity.java
@@ -0,0 +1,42 @@
+package com.shop.api_gateway.entity.log;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "logs")
+public class LogEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDateTime timestamp;
+
+    @Column(length = 50)
+    private String logLevel;
+
+    @Column(length = 100)
+    private String serviceName;
+
+    @Column(length = 255)
+    private String operation;
+
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID userId;
+
+    @Lob
+    @Column
+    private String message;
+
+    @Lob
+    @Column
+    private String stackTrace;
+
+
+}
